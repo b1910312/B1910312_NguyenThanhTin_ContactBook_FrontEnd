c5dadbf8a919fac846b3627ee7cebc9995505781
Nguyen Thanh Tin <tinb1910312@student.ctu.edu.vn>
2023-04-03 23:24:57 +0700

    Tao trang them lien he

---
+++ src/views/ContactAdd.vue
@@ -0,0 +1,46 @@
+<template>
+    <div v-if="contact" class="page">
+        <h4>Thêm Liên hệ</h4>
+        <ContactForm :contact="contact" @submit:contact="addContact" />
+        <p>{{ message }}</p>
+    </div>
+</template>
+<script>
+import ContactForm from "@/components/ContactForm.vue";
+import ContactService from "@/services/contact.service";
+export default {
+    components: {
+        ContactForm,
+    },
+    data() {
+        return {
+            contact: "",
+            message: "",
+        };
+    },
+    methods: {
+        async NullContact() {
+
+            this.contact = {
+                "address": "",
+                "email": "",
+                "favorite": "",
+                "name": "",
+                "phone": ""
+            }
+        },
+        async addContact(data) {
+            try {
+                await ContactService.create(data);
+                this.message = "Liên hệ được thêm thành công.";
+            } catch (error) {
+                console.log(error);
+            }
+        }
+    },
+    created() {
+        this.NullContact();
+        this.message = "";
+    },
+};
+</script>
